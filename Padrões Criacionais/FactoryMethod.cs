ee4797e7453fc68f641b3ed451c9e64414313ac2
Daniel Toledo <dan992965@gmail.com>
2024-04-06 19:55:26 -0300

    FactoryMethod

---
+++ Padrões Criacionais/FactoryMethod.cs
@@ -0,0 +1,5 @@
+// Factory Method
+
+// O Factory Method é um padrão criacional de projeto que fornece uma interface para criar objetos em uma superclasse, 
+// mas permite que as subclasses alterem o tipo de objetos que serão criados.
+
